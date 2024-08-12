b502155ff3561e44b4447119b7d4747524fc84df
MJHamar <Miklos.Hamar@warwick.ac.uk>
2024-08-12 18:56:46 +0200

    inserted layout and managed to overlay the bookshelf component

--- books-web/components/BookshelfView.js
+++ books-web/components/BookshelfView.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Bookshelf } from '../components/Bookshelf';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const ImageViewer = () => {
+    const books = [
+        {
+            title: "The Great Gatsby",
+            author: "F. Scott Fitzgerald",
+            date: "1925",
+            rating: 4.5,
+            coverImage: "/path/to/great-gatsby.jpg", // Ensure you have an image here
+            spineColor: "#1b1b1b",
+            textColor: "#fff",
+            slug: "the-great-gatsby",
+            summary: "A novel about the American dream...",
+        },
+        // Add more book objects here
+    ];
+
     return (
-        <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+        <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', position: 'relative' }}>
             <TransformWrapper
                 defaultScale={1}
                 wheel={{ step: 0.2 }} // Mouse scroll for zooming
@@ -11,13 +27,19 @@ const ImageViewer = () => {
             >
                 <TransformComponent>
                     <img
-                        src="/img/everest.png"
+                        src="/img/layout1.png"
                         alt="Large View"
-                        style={{ width: '200%', height: 'auto', cursor: 'grab' }} // Adjust as needed
+                        style={{
+                            width: '2048px', height: 'auto', cursor: 'grab', position: 'relative', top: '0px', left: '0px', zIndex: 1 }}
                     />
+                    <div style={{ position: 'absolute', top: '10px', left: '10px', zIndex: 100 }}>
+                        <Bookshelf books={books} />
+                    </div>
                 </TransformComponent>
+
             </TransformWrapper>
         </div>
+
     );
 };
 
--- books-web/pages/index.js
+++ books-web/pages/index.js
@@ -1,28 +1,13 @@
-import { Bookshelf } from '../components/Bookshelf';
+
 import dynamic from 'next/dynamic';
 
 const ImageViewer = dynamic(() => import('../components/BookshelfView'), { ssr: false });
 
 
 const Home = () => {
-  const books = [
-    {
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-      date: "1925",
-      rating: 4.5,
-      coverImage: "/path/to/great-gatsby.jpg", // Ensure you have an image here
-      spineColor: "#1b1b1b",
-      textColor: "#fff",
-      slug: "the-great-gatsby",
-      summary: "A novel about the American dream...",
-    },
-    // Add more book objects here
-  ];
 
   return (
     <div>
-      <Bookshelf books={books} />
       <ImageViewer />
     </div>
   );
